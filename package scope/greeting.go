0cba62276123686524b02ad627e884af8deb098a
jahidulhasanzahid <2017zahidhasan@gmail.com>
2023-02-22 00:25:32 +0600

    package scope

---
+++ package scope/greeting.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var points = []int{23, 34, 45, 67, 87}
+
+func sayHello(name string) {
+	fmt.Println("Hello", name)
+}
+
+func showScore() {
+	fmt.Println("The score is ", score)
+}
---
+++ package scope/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var score = 23.45
+
+func main() {
+	sayHello("Jahidul")
+
+	for _, value := range points {
+		fmt.Println(value)
+	}
+
+	showScore()
+}
